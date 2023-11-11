57204fc496c23c6cd124e9741c1230eaa6ecef19
Emmanuel Sampaio <emmanuellima07@alu.ufc.br>
2023-11-11 16:24:58 -0300

    adicionando o slider

---
+++ script.js
@@ -0,0 +1,18 @@
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval(function () {
+    nextImage();
+}, 5000);
+
+function nextImage() {
+    count++;
+    if (count > 3) {
+        count = 1;
+    }
+    document.getElementById("radio" + count).checked = true;
+
+    
+    var event = new Event('change');
+    document.getElementById("radio" + count).dispatchEvent(event);
+}
